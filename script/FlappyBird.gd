c03158b339793d8998ee72d4e62740a1bddd98b6
mehmet kesik <livered31@gmail.com>
2019-07-22 23:18:16 +0300

    commit

--- script/FlappyBird.gd
+++ script/FlappyBird.gd
@@ -4,17 +4,7 @@ var speed = 2
 var score = 0
 
 func _ready():
-	#restartGame()
 	pass
 
-func restartGame():
-	var startCount = get_node("schbox/scvbox/startCount")
-	get_tree().paused = true
-	yield(get_tree().create_timer(1), "timeout")
-	startCount.text = "2"
-	yield(get_tree().create_timer(1), "timeout")
-	startCount.text = "1"
-	yield(get_tree().create_timer(1), "timeout")
-	startCount.visible = false
-	get_tree().paused = false
-	pass
+func _on_speedTimer_timeout():
+	speed+=1
--- script/floor.gd
+++ script/floor.gd
@@ -4,14 +4,6 @@ func _ready():
 	var floor1 = get_node("floor1")
 	var floor2 = get_node("floor2")
 	var floor3 = get_node("floor3")
-	var x_fark = floor1.texture.get_size().x
-	var y_fark = floor1.texture.get_size().y * scale.y
-	floor2.position.x = floor1.position.x + x_fark
-	floor3.position.x = floor2.position.x + x_fark
-	position.y = get_viewport().size.y -  (y_fark/2)
-	position.x = (floor1.texture.get_size().x * scale.x)/2
-	pass
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+	floor2.position.x = floor1.position.x + floor1.texture.get_size().x
+	floor3.position.x = floor2.position.x + floor1.texture.get_size().x
+	position.x = floor1.texture.get_size().x/2
